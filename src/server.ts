71191c8a367ca0879037d004a0c0e8811bd95032
Fellipe Neves <fellipe2.0e@gmail.com>
2023-02-25 12:11:00 -0300

    Create hard application

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql";
+import { AppointmentResolver } from "./resolvers/appointments-resolver";
+import path from "node:path";
+
+
+async function bootstrap() {
+
+    const schema = await buildSchema({
+        resolvers: [
+            AppointmentResolver
+        ],
+        emitSchemaFile: path.resolve(__dirname, "schema.gql")
+    })
+
+
+    const server = new ApolloServer({
+        schema
+    });
+
+    const {url} = await server.listen();
+
+    console.log(`HTTP server running... ${url}`);
+
+}
+
+bootstrap();
