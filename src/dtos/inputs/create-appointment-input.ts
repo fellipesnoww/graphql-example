10ca9fa2a758f24608738f01bec1615e58aba37f
Fellipe Neves <fellipe2.0e@gmail.com>
2023-02-25 12:19:23 -0300

    fix type

--- src/dtos/inputs/create-appointment-input.ts
+++ src/dtos/inputs/create-appointment-input.ts
@@ -3,7 +3,7 @@ import {InputType, Field} from "type-graphql";
 @InputType()
 export class CreateAppointmentInput{
     @Field()
-    customerId: string;
+    customerId: String;
     
     @Field()
     startsAt: Date;
